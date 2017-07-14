6103b003637c8281c5f1092cdc5e49a83e9ab3c3
AFinalStone <602392033@qq.com>
2017-07-14 18:21:25 +0800

    集成H5到项目中

--- app/src/main/java/com/shi/androidstudy/myfirsth5/MainActivity.java
+++ app/src/main/java/com/shi/androidstudy/myfirsth5/MainActivity.java
@@ -1,13 +1,45 @@
 package com.shi.androidstudy.myfirsth5;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.FrameLayout;
+
+import io.dcloud.EntryProxy;
+import io.dcloud.WebviewModeListener;
+import io.dcloud.feature.internal.sdk.SDK;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EntryProxy mEntryProxy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        if(mEntryProxy == null){
+
+            FrameLayout rootView = new FrameLayout(this);
+
+            // 创建5+内核运行事件监听
+
+            WebviewModeListener wm = new WebviewModeListener(this,rootView);
+
+            // 初始化5+内核
+
+            mEntryProxy = EntryProxy.init(this,wm);
+
+            // 启动5+内核，并指定内核启动类型
+
+            mEntryProxy.onCreate(savedInstanceState, SDK.IntegratedMode.WEBVIEW,null);
+
+            setContentView(rootView);
+
+        }
+
     }
 }
